f224d3b9da8e7ea7ce454836a0e9ccd8a3e47c9b
Bloemheuvel <s.d.bloemheuvel@tue.nl>
2021-12-14 12:55:54 +0100

    Update sample_rate.py

--- sample_rate.py
+++ sample_rate.py
@@ -16,3 +16,15 @@ def return_sample_rate():
 
     
     st.markdown('This means that every second, each variable in your dataset takes a measurement.')
+    
+    st.markdown('It could, however, also be the case that your data looks like the following:')
+    
+    st.table(pd.DataFrame({
+            'Time': ['21-12-21 10:00:00', '21-12-21 10:05:00','21-12-21 10:10:00','21-12-21 10:15:00'],
+            'Sensor1': [10, 10, 11, 10],
+            'Sensor2': [14,15,14,14]
+        }))
+    
+    st.markdown('Which means that every 5 minutes your data is recorded.')
+    
+    st.warning('It depends on your goal wheter or not every second or every 5 minutes is prefered')
