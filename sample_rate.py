6f59f543893a8e33b3ae393426efd7aa0676bda3
Bloemheuvel <s.d.bloemheuvel@tue.nl>
2021-12-14 12:53:38 +0100

    Update sample_rate.py

--- sample_rate.py
+++ sample_rate.py
@@ -10,8 +10,9 @@ def return_sample_rate():
     
     st.table(pd.DataFrame({
             'Time': ['21-12-21 10:00:00', '21-12-21 10:00:01','21-12-21 10:00:02','21-12-21 10:00:03'],
-            'Sensor1': [10, 10, 11, 10]
+            'Sensor1': [10, 10, 11, 10],
+            'Sensor2': [14,15,14,14]
         }))
 
     
-    
+    st.markdown('This means that every second, each variable in your dataset takes a measurement.')
