79761d6362a15f90f25465a9a7821eee85cf0e0a
Bloemheuvel <s.d.bloemheuvel@tue.nl>
2021-12-14 12:43:50 +0100

    Update number_of_variables.py

--- number_of_variables.py
+++ number_of_variables.py
@@ -9,11 +9,23 @@ def return_number_of_variables():
     
     option = st.selectbox(
         'Select the number of variables in your dataset',
-        ['','<10','10-20','21-100','100+'], format_func=lambda x: 'Select an option' if x == '' else x)
+        ['','<10','10-30','31+'], format_func=lambda x: 'Select an option' if x == '' else x)
     
     
     if option == '<10':
         st.success('You have an ideal number of variables!')
         st.write("You have only collected the important variables that belong to your goal.")
         
+    if option == '10-30':
+        st.warning('You are in the safe zone, but keep in mind that the number of variables can be reduced further')
+        
+    if option == '31+':
+        st.error('Too many variables!')
+        st.markdown('Here are some tips to reduce the number of variables')
+        st.success('Try finding variables that do not contain any deviation')
+        st.markdown('''Most likely, quite a lot of your variables are set variables.
+                    What we mean by set variables are variables that monitor the settings of a machine.
+                    For example, the set variable of temperature can be 90 degrees, and the variables that measures temperature has the actual readings 90.1, 90.0, 89.9, 90 etc.
+                    
+                    ''')
     
