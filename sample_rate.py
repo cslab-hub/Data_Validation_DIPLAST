c491b72e3b87defdc13040da955eaae3969da2c4
Bloemheuvel <s.d.bloemheuvel@tue.nl>
2021-12-15 09:17:55 +0100

    Update sample_rate.py

--- sample_rate.py
+++ sample_rate.py
@@ -3,6 +3,14 @@ import pandas as pd
 import numpy as np
 from PIL import Image 
 
+def highlight_cols(x):
+    # copy df to new - original data is not changed
+    df = x.copy()
+    # select all values to yellow color
+    df.loc[:, 0] = 'background-color: yellow'
+    # return color df
+    return df 
+
 def return_sample_rate():
     
     st.title('Determine at what moments in time an observation is made.')
@@ -12,7 +20,7 @@ def return_sample_rate():
             'Time': ['21-12-21 10:00:00', '21-12-21 10:00:01','21-12-21 10:00:02','21-12-21 10:00:03'],
             'Sensor1': [10, 10, 11, 10],
             'Sensor2': [14,15,14,14]
-        }))
+        }).style.apply(highlight_cols, axis=None))
 
     
     st.markdown('This means that every second, each variable in your dataset takes a measurement.')
