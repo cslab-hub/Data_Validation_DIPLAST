16b4248f9bfec143dec3a96020c0a152ffb07705
StefanBloemheuvel <stefanbloemheuvel@gmail.com>
2021-12-16 11:54:35 +0100

    added requiremets

--- outliers.py
+++ outliers.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from PIL import Image 
 import matplotlib.pyplot as plt 
-from sklearn.neighbors import LocalOutlierFactor
+# from sklearn.neighbors import LocalOutlierFactor
 
 data = pd.read_csv('https://raw.githubusercontent.com/DHI/tsod/main/tests/data/example.csv')
 data.to_csv('data/anomaly.csv', index=None)
@@ -12,12 +12,7 @@ fig = plt.plot(data['value'])
 fig, ax = plt.subplots()
 ax.plot(data['value'])
 
-import tsod
 
-rd = tsod.RangeDetector(min_value=0.01, max_value=2.0)
-
-res = rd.detect(data['value'])
-data[res]
 
 
 
